a372b2349558bd3dc564cde6cf9a62360d493ffd
ewallace <ewallace@berkleyre.com.au>
2016-11-29 11:19:50 +1100

    more tests, gitignore

---
+++ src/Translators/NumberTranslator.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Translators
+{
+    public class NumberTranslator
+    {
+        public string Translate(string numberString)
+        {
+            int number;
+            string returnValue = "";
+            if (!int.TryParse(numberString, out number))
+            {
+                throw new ArgumentException("value entered cannot be converted to a number");
+            };
+
+
+            // var numOfGroups = filledArray.Length / 3
+
+            var filledArray = FillInArray(numberString.ToCharArray());
+
+            while(filledArray.Length != 0)
+            {
+                returnValue += HandleDigitGrouping(new char[] { filledArray[i], filledArray[i + 1], filledArray[i + 2]});
+            }
+
+
+            // for (var i = 1; i <= numOfGroups; i += 3)
+            // {
+            //     returnValue += HandleDigitGrouping(new char[] { filledArray[i], filledArray[i + 1], filledArray[i + 2]});
+            //     returnValue += GetDigitGroupDescription(1);
+            // }
+
+            return returnValue;
+        }
+
+        public int GetNumberOfDigitGroups(int arrayLength)
+        {
+            if (arrayLength % 3 != 0)
+            {
+                throw new ArgumentException("This should be a modulo of 3");
+            }
+
+            // Remove 1 because hundreds ared handled in GetHundreds;
+            int returnValue = arrayLength /3;
+            
+            if(returnValue == 1)
+            {
+                return returnValue;
+            }
+            return returnValue - 1;
+        }
+
+        public string HandleDigitGrouping(char[] array)
+        {
+            if (array.Length != 3)
+            {
+                throw new ArgumentException("This should be 3");
+            }
+
+            string returnValue = "";
+
+            if (array[0] != '0')
+            {
+                returnValue += GetHundreds(array[0]) + " and ";
+            }
+
+            return returnValue += GetTens(new char[] { array[1], array[2] });
+        }
+
+        public char[] FillInArray(char[] array)
+        {
+            // Array Length + 3 makes this work with arrays containing less than 3 items
+            int arrayLength = array.Length + 3;
+            // Convert to list, because arrays blow to work with
+            var returnValue = new List<char>();
+
+            if (arrayLength % 3 == 2)
+            {
+                returnValue.Add('0');
+                returnValue.AddRange(array.ToList());
+            }
+            else if (arrayLength % 3 == 1)
+            {
+                returnValue.Add('0');
+                returnValue.Add('0');
+                returnValue.AddRange(array.ToList());
+            }
+            else
+            {
+                returnValue.AddRange(array.ToList());
+            }
+
+            return returnValue.ToArray();
+        }
+
+        public string GetHundreds(char digit)
+        {
+            var number = int.Parse(digit.ToString());
+            return TranslateSingles(number) + " Hundred";
+        }
+
+        public string GetTens(char[] digit)
+        {
+
+            if (digit.Length > 2)
+            {
+                throw new ArgumentException("Cannot convert to Tens from less than 2 digits");
+            }
+
+            var number = int.Parse(digit[0].ToString() + digit[1].ToString());
+            var digit1 = int.Parse(digit[0].ToString());
+            var digit2 = int.Parse(digit[1].ToString());
+            string space = "";
+
+            if (digit2 != 0) space = " ";
+
+            if (number < 10)
+            {
+                return TranslateSingles(number);
+            }
+
+            if (number < 20)
+            {
+                return TranslateTeens(number);
+            }
+
+            return TranslateSingles(digit1, true) + space + TranslateSingles(digit2);
+        }
+
+        public string TranslateSingles(int number, bool tens = false)
+        {
+            switch (number)
+            {
+                case 1: return "One";
+                case 2: return tens ? "Twenty" : "Two";
+                case 3: return tens ? "Thirty" : "Three";
+                case 4: return tens ? "Fourty" : "Four";
+                case 5: return tens ? "Fifty" : "Five";
+                case 6: return tens ? "Sixty" : "Six";
+                case 7: return tens ? "Seventy" : "Seven";
+                case 8: return tens ? "Eighty" : "Eight";
+                case 9: return tens ? "Ninety" : "Nine";
+                default: return "";
+            }
+        }
+
+        public string TranslateTeens(int number)
+        {
+            switch (number)
+            {
+                case 10: return "Ten";
+                case 11: return "Eleven";
+                case 12: return "Twelve";
+                case 13: return "Thirteen";
+                case 14: return "Fourteen";
+                case 15: return "Fifteen";
+                case 16: return "Sixteen";
+                case 17: return "Seventeen";
+                case 18: return "Eighteen";
+                case 19: return "Nineteen";
+                default: return "";
+            }
+        }
+
+        public string GetDigitGroupDescription(int number)
+        {
+            switch (number)
+            {
+                case 2: return "Thousand ";
+                case 3: return "Million ";
+                case 4: return "Billion ";
+                case 5: return "Trillion ";
+                default: return "";
+            }
+        }
+    }
+}
---
+++ test/Translators.Tests/NumberTranslatorTests.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Xunit;
+using Shouldly;
+
+namespace Translators.Tests
+{
+    public class NumberTranslatorTests
+    {
+        private readonly NumberTranslator _numberTranslater;
+        public NumberTranslatorTests()
+        {
+            _numberTranslater = new NumberTranslator();
+        }
+
+        [Fact]
+        public void Translate_IntEntered_StringReturned()
+        {
+            var result = _numberTranslater.Translate("6");
+            result.ShouldBe("6");
+        }
+
+        [Fact]
+        public void Translate_InvalidStringEtnered_ArgumentExceptionThrown()
+        {
+            Assert.Throws<ArgumentException>(() => _numberTranslater.Translate("Hello"));
+        }
+
+        [Theory]
+        [InlineData(new char[] { '1' }, new char[] { '0', '0', '1' })]
+        [InlineData(new char[] { '1', '2' }, new char[] { '0', '1', '2' })]
+        [InlineData(new char[] { '1', '2', '3' }, new char[] { '1', '2', '3' })]
+        [InlineData(new char[] { '1', '2', '3', '4' }, new char[] { '0', '0', '1', '2', '3', '4' })]
+        [InlineData(new char[] { '1', '2', '3', '4', '5' }, new char[] { '0', '1', '2', '3', '4', '5' })]
+        [InlineData(new char[] { '9', '9', '9', '9', '9'}, new char[] { '0', '9', '9', '9', '9', '9'})]
+        public void FillInArray_DoesWhat_YouExpect(char[] entered, char[] expected)
+        {
+            var result = _numberTranslater.FillInArray(entered);
+            result.ShouldBe(expected);
+        }
+
+        [Theory]
+        // [InlineData(new char[] { '0', '1' }, "One")]
+        // [InlineData(new char[] { '0', '8' }, "Eight")]
+        [InlineData(new char[] { '1', '2' }, "Twelve")]
+        [InlineData(new char[] { '2', '3' }, "Twenty Three")]
+        [InlineData(new char[] { '3', '4' }, "Thirty Four")]
+        [InlineData(new char[] { '4', '5' }, "Fourty Five")]
+        public void DoMagic_DoesMagic(char[] entered, string expected)
+        {
+            var result = _numberTranslater.GetTens(entered);
+            result.ShouldBe(expected);
+        }
+
+        [Theory]
+        [InlineData(new char[] { '0', '1', '2' }, "Twelve")]
+        [InlineData(new char[] { '2', '3', '0' }, "Two Hundred and Thirty")]
+        [InlineData(new char[] { '3', '1', '4' }, "Three Hundred and Fourteen")]
+        [InlineData(new char[] { '4', '5', '7' }, "Four Hundred and Fifty Seven")]
+        [InlineData(new char[] { '9', '9', '9' }, "Nine Hundred and Ninety Nine")]
+        public void HandleDigitGrouping_WorksProperly(char[] entered, string expected)
+        {
+            var result = _numberTranslater.HandleDigitGrouping(entered);
+            result.ShouldBe(expected);
+        }
+
+
+                [Theory]
+        // [InlineData("12", "Twelve")]
+        // [InlineData("230", "Two Hundred and Thirty")]
+        // [InlineData("314", "Three Hundred and Fourteen")]
+        // [InlineData("457", "Four Hundred and Fifty Seven")]
+        [InlineData("9999", "Nine Thousand Nine Hundred and Ninety Nine")]
+        [InlineData("99999", "Ninety Nine Thousand Nine Hundred and Ninety Nine")]
+        [InlineData("999999", "Nine Million Nine Hundred and Ninety Nine Thousand Nine Hundred and Ninety Nine")]
+        public void Translate_Works(string entered, string expected)
+        {
+            var result = _numberTranslater.Translate(entered);
+            result.ShouldBe(expected);
+        }
+
+    }
+}
